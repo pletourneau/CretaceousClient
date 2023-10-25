e3f5d8f3c824fb53e046f809e1eade44df57992f
Paul LeTourneau <pletourneau62@gmail.com>
2023-10-25 16:12:34 -0700

    MVP
    Co-authored-by: Moshe Atia <moshikoatia@gmail.com>
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>
    Co-authored-by: Jon Cheng <joncheng.dev@gmail.com>

--- CretaceousClient/Models/Animal.cs
+++ CretaceousClient/Models/Animal.cs
@@ -12,15 +12,44 @@ namespace CretaceousClient.Models
     public string Species { get; set; }
     public int Age { get; set; }
 
+    // We need two very similar "Get" methods because they return two different types of data, one a list, and one a string.
     public static List<Animal> GetAnimals()
     {
-      var apiCallTask = ApiHelper.GetAll();
+      var apiCallTask = ApiHelper.GetAll(); // uses "GetAll" ApiHelper method
       var result = apiCallTask.Result;
 
-      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result); // Results in a JSON array
       List<Animal> animalList = JsonConvert.DeserializeObject<List<Animal>>(jsonResponse.ToString());
 
       return animalList;
+    }
+
+    public static Animal GetDetails(int id) // requires id parameter to get a specific animal
+    {
+      var apiCallTask = ApiHelper.Get(id); // uses "Get" ApiHelper method
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result); // Results in a JSON object
+      Animal animal = JsonConvert.DeserializeObject<Animal>(jsonResponse.ToString());
+
+      return animal;
+    }
+
+    public static void Post(Animal animal)
+    {
+      string jsonAnimal = JsonConvert.SerializeObject(animal);
+      ApiHelper.Post(jsonAnimal);
+    }
+
+    public static void Put(Animal animal)
+    {
+      string jsonAnimal = JsonConvert.SerializeObject(animal);
+      ApiHelper.Put(animal.AnimalId, jsonAnimal);
+    }
+
+    public static void Delete(int id) 
+    {
+      ApiHelper.Delete(id);
     }
   }
 }
--- CretaceousClient/Models/ApiHelper.cs
+++ CretaceousClient/Models/ApiHelper.cs
@@ -12,5 +12,39 @@ namespace CretaceousClient.Models
       RestResponse response = await client.GetAsync(request);
       return response.Content;
     }
+
+    public static async Task<string> Get(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5000/");
+      RestRequest request = new RestRequest($"api/animals/{id}", Method.Get);
+      RestResponse response = await client.GetAsync(request);
+      return response.Content;
+    }
+
+    public static async void Post(string newAnimal)
+    {
+      RestClient client = new RestClient("http://localhost:5000/");
+      RestRequest request = new RestRequest($"api/animals", Method.Post);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newAnimal);
+      await client.PostAsync(request);
+    }
+
+    public static async void Put(int id, string animalToEdit) // Changed "string newAnimal" to more appropriate name.
+    {
+      RestClient client = new RestClient("http://localhost:5000/");
+      RestRequest request = new RestRequest($"api/animals/{id}", Method.Put);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(animalToEdit);
+      await client.PutAsync(request);
+    }
+
+    public static async void Delete(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5000/");
+      RestRequest request = new RestRequest($"api/animals/{id}", Method.Delete);
+      request.AddHeader("Content-Type", "application/json");
+      await client.DeleteAsync(request);
+    }
   }
 }
